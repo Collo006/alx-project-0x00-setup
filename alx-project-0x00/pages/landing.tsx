374297dfa441db711825918caea3ee3c5667b6fe
Collins Kipruto <collinstunge005@gmail.com>
2025-07-16 18:12:01 +0300

    submit

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,10 +6,20 @@ const Landing: React.FC = () =>{
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button title="Button" styles=" rounded-sm "/>
-      <Button title="Button" styles=" rounded-md " />
-      <Button title="Button" styles=" rounded-full " />
-    </div>
+   {/* Small Buttons */}
+      <Button title="Small + rounded-sm" styles="text-sm rounded-sm" />
+      <Button title="Small + rounded-md" styles="text-sm rounded-md" />
+      <Button title="Small + rounded-full" styles="text-sm rounded-full" />
+
+      {/* Medium Buttons */}
+      <Button title="Medium + rounded-sm" styles="text-base rounded-sm" />
+      <Button title="Medium + rounded-md" styles="text-base rounded-md" />
+      <Button title="Medium + rounded-full" styles="text-base rounded-full" />
+
+      {/* Large Buttons */}
+      <Button title="Large + rounded-sm" styles="text-lg rounded-sm" />
+      <Button title="Large + rounded-md" styles="text-lg rounded-md" />
+      <Button title="Large + rounded-full" styles="text-lg rounded-full" />    </div>
   )
 }
 export default Landing
